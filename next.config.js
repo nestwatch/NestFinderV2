de06e4d37b5d5897287c8eb23327c1ed51db5980
shijin jayaprakash <shijin@nest-watch.com>
2024-08-09 18:47:23 -0400

    Added latest changes

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config, { dev, isServer }) {
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;  
